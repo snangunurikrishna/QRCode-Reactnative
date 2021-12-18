5023c51b1ccc1a1fcdb4414c5e791c2a4e1b0cd7
snangunurikrishna <nsaikrishna008@gmail.com>
2021-12-18 20:17:37 +0530

    First version: ======== =>Contains Basic code for generating qr code with button, Qrcode, and text input =>No navigation for now

---
+++ App.js
@@ -0,0 +1,105 @@
+import React, {useState, useEffect} from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import {
+    SafeAreaView,
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+  } from 'react-native';
+
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [qrvalue, setQrvalue] = useState('');
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+      <Text style={styles.titleStyle}>
+          React Native Tutorials: Part 1
+        </Text>
+        <Text style={styles.titleStyle}>
+          React Native Tutorials: Generation of QR Code in React Native
+        </Text>
+        <QRCode
+          value={qrvalue ? qrvalue : 'NA'}
+          size={250}
+          color="black"
+          backgroundColor="white"
+          logo={{
+            url:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/logosmalltransparen.png',
+          }}
+          logoSize={30}
+          logoMargin={2}
+          logoBorderRadius={15}
+          logoBackgroundColor="yellow"
+        />
+        <Text style={styles.textStyle}>
+          Please insert any value to generate QR code
+        </Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={
+            (inputText) => setInputText(inputText)
+          }
+          placeholder="Enter Any Value here"
+          value={inputText}
+        />
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => setQrvalue(inputText)}>
+          <Text style={styles.buttonTextStyle}>
+            Generate QR Code
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+  },
+  titleStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    margin: 10,
+  },
+  textInputStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#51D8C7',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#51D8C7',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 30,
+    padding: 10,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
